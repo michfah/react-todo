1d0b2ae46c0627049e3683be71fce0b267649255
Michael Fahey <michael@perception.ie>
2018-04-05 11:08:44 +0100

    Create AddTodo component

---
+++ app/tests/components/TodoItem.test.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var TestUtils = require('react-addons-test-utils');
+var expect = require('expect');
+var $ = require('jquery');
+
+var TodoItem = require('TodoItem');
+
+describe('TodoItem', () => {
+  it('should exist', () => {
+    expect(TodoItem).toExist();
+  });
+});
