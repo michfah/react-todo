7a1d14f5fe97270ea7e77ea9a26bf0be648d9753
Michael Fahey <michael@perception.ie>
2018-04-05 15:01:28 +0100

    Add filtering functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -20,11 +20,32 @@ module.exports = {
     }
 
     return $.isArray(todos) ? todos : [];
+  },
+  filterTodos: function(todos, showCompletedTodos, searchText) {
+    var filteredTodos = todos;
 
-    // if ($.isArray(todos)) {
-    //   return todos;
-    // } else {
-    //   return [];
-    // }
+    // filter by showCompletedTodos
+    filteredTodos = filteredTodos.filter((todo) => {
+      return !todo.completed || showCompletedTodos;
+    });
+
+    // filter by searchText
+    filteredTodos = filteredTodos.filter((todo) => {
+      var text = todo.text.toLowerCase();
+      return searchText.length === 0 || text.indexOf(searchText) > -1;
+    });
+
+    // sort todos with non-completed first
+    filteredTodos.sort((a, b) => {
+      if (!a.completed && b.completed) {
+        return -1;
+      } else if (a.completed && !b.completed) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+
+    return filteredTodos;
   }
 };
--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -47,12 +47,13 @@ var TodoApp = React.createClass({
     });
   },
   render: function() {
-    var {todos} = this.state;
+    var {todos, showCompletedTodos, searchText} = this.state;
+    var filteredTodos = TodoAPI.filterTodos(todos, showCompletedTodos, searchText);
 
     return (
       <div>
         <TodoSearch onSearch={this.handleSearch}/>
-        <TodoList todos={todos} onToggle={this.handleToggle}/>
+        <TodoList todos={filteredTodos} onToggle={this.handleToggle}/>
         <AddTodo onAddTodo = {this.handleAddTodo}/>
       </div>
     )
